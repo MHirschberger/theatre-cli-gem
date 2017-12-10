52cd5fbd6c6c35eb819f6d0422a9bbc17434df08
Michael Hirschberger <mlhirschberger@gmail.com>
2017-12-10 22:09:23 +0000

    Updating the cli.rb file

--- lib/theatre_cli_gem/cli.rb
+++ lib/theatre_cli_gem/cli.rb
@@ -3,6 +3,7 @@ class TheatreCliGem::CLI
   def call
     list_shows
     menu
+    goodbye
   end
 
   def list_shows
@@ -10,12 +11,28 @@ class TheatreCliGem::CLI
     puts <<-DOC
       1. The Phantom of the Opera
       2. Dear Evan Hansen
-      3. Cats
-      4. A Bronx Tale
     DOC
   end
 
   def menu
-    puts "Enter the number of the show you're interested in learning more about:"
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the show you're interested in learning more about or 'list' for the list of current shows:"
+      input = gets.strip.downcase
+      case input
+        when "1"
+          puts "More info on deal 1..."
+        when "2"
+          puts "More info on deal 2..."
+        when "list"
+          list_shows
+        else
+          puts "Type 'exit' to leave or 'list' to list Broadway shows currently playing:"
+      end
+    end
+  end
+
+  def goodbye
+    puts "We hope you found the information you were seeking. Enjoy!"
   end
 end
