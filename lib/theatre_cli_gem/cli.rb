71b9f37f7159a0c324a69111ba6a048a7d8e05ee
Michael Hirschberger <mlhirschberger@gmail.com>
2018-02-28 00:56:03 +0000

    Updated cli.rb file.

--- lib/theatre_cli_gem/cli.rb
+++ lib/theatre_cli_gem/cli.rb
@@ -24,7 +24,7 @@ class TheatreCliGem::CLI
       elsif input == "list"
         list_shows
       else
-        puts "Invalid entry.\nType 'list' to list Broadway shows currently playing or type 'exit' to leave:"
+        puts "Invalid entry.\nType 'list' to list Broadway shows currently playing or type 'exit' to leave:" if input != "exit"
       end
     end
   end
